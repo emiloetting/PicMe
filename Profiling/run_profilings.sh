b19b6a214fba5c42a6e1f28fc59f42d4d0f472f3
Emil Ötting <emil.oetting@gmx.de>
2025-08-14 14:27:11 +0200

    Change linebreak-style

--- Profiling/run_profilings.sh
+++ Profiling/run_profilings.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # Create output directory if it doesn't exist
 mkdir -p Profiling/profiles_html
 
 # Single img color based profiling
 .venv/Scripts/python.exe -m cProfile -o Profiling/profiles_html/color_single.prof Profiling/color_single_profiling.py
 .venv/Scripts/python.exe Profiling/snakeviz_static_html.py Profiling/profiles_html/color_single.prof
-
 # 2 img color based profiling
+
 .venv/Scripts/python.exe -m cProfile -o Profiling/profiles_html/color_double_unextreme.prof Profiling/color_double_profiling_unextreme_weightened.py
 .venv/Scripts/python.exe Profiling/snakeviz_static_html.py Profiling/profiles_html/color_double_unextreme.prof
