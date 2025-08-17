59a398c3626da66b93989c0f2e6e2f38980b623f
Emil Ötting <emil.oetting@gmx.de>
2025-08-17 22:26:53 +0200

    cleanup & bring up to date

--- Profiling/color_double_profiling_unextreme_weightened.py
+++ Profiling/color_double_profiling_unextreme_weightened.py
@@ -1,23 +1,18 @@
 import os
-import sys
 import numpy as np
 from ColorSimilarity.main_helper import *
-from ColorSimilarity.colorClusterquantized import L_BINS, A_BINS, B_BINS
+from Initialization.setup import L_BINS, A_BINS, B_BINS
 
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    full_hists_L1 = os.path.join(cwd, 'ColorSimilarity', 'FullHists_L1.npy')
-    image_path_1 = os.path.join(cwd, 'Profiling', 'test_img_1.jpg')
-    image_path_2 = os.path.join(cwd, 'Profiling', 'test_img_2.jpg')
-    cost_mat_path = os.path.join(cwd, "ColorSimilarity", 'CostMatrix_full.npy')
-    ann_idx_path = os.path.join(cwd, 'ColorSimilarity', 'color_index_l2.ann')
-    image_data_root = os.path.join(cwd, 'ImageData')
-    database_image_paths = [os.path.join(image_data_root, f) for f in os.listdir(image_data_root) if os.path.isfile(os.path.join(image_data_root, f))]  
+    image_path_1 = os.path.join(cwd,'Profiling', 'test_img_1.jpg')
+    image_path_2 = os.path.join(cwd,'Profiling', 'test_img_2.jpg')
+    cost_mat_path = os.path.join(cwd, "DataBase", 'emd_cost_full.npy')
+    ann_idx_path = os.path.join(cwd, 'DataBase', 'color_ann_index.ann')   
 
-    # Load cost matrix for EMD & L1 normalized backend vecs
+    # Load cost matrix for EMD
     cost_matrix = np.load(cost_mat_path)
-    M_V_L1 = np.load(full_hists_L1)
 
     # Load ANN tree
     l2_index = ann.AnnoyIndex(L_BINS*A_BINS*B_BINS, 'angular')
@@ -25,8 +20,6 @@ if __name__ == "__main__":
 
     color_match_double_ann(
         img_paths=[image_path_1, image_path_2],
-        db_img_paths=database_image_paths,
-        l1_emb=M_V_L1,
         annoy_index=l2_index,
         l_bins=L_BINS,
         a_bins=A_BINS,
--- Profiling/color_single_profiling.py
+++ Profiling/color_single_profiling.py
@@ -1,22 +1,17 @@
 import os
-import sys
 import numpy as np
 from ColorSimilarity.main_helper import *
-from ColorSimilarity.colorClusterquantized import L_BINS, A_BINS, B_BINS
+from Initialization.setup import L_BINS, A_BINS, B_BINS
 
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    full_hists_L1 = os.path.join(cwd, 'ColorSimilarity', 'FullHists_L1.npy')
     image_path = os.path.join(cwd,'Profiling', 'test_img_1.jpg')
-    cost_mat_path = os.path.join(cwd, "ColorSimilarity", 'CostMatrix_full.npy')
-    ann_idx_path = os.path.join(cwd, 'ColorSimilarity', 'color_index_l2.ann')
-    image_data_root = os.path.join(cwd, 'ImageData')
-    database_image_paths = [os.path.join(image_data_root, f) for f in os.listdir(image_data_root) if os.path.isfile(os.path.join(image_data_root, f))]  
+    cost_mat_path = os.path.join(cwd, "DataBase", 'emd_cost_full.npy')
+    ann_idx_path = os.path.join(cwd, 'DataBase', 'color_ann_index.ann')  
 
-    # Load cost matrix for EMD & L1 normalized backend vecs
+    # Load cost matrix for EMD 
     cost_matrix = np.load(cost_mat_path)
-    M_V_L1 = np.load(full_hists_L1)
 
     # Load ANN tree
     l2_index = ann.AnnoyIndex(L_BINS*A_BINS*B_BINS, 'angular')
@@ -25,8 +20,6 @@ if __name__ == "__main__":
     # Perform actual similarity search
     color_match_single_ann(
         img_path=[image_path],
-        db_img_paths=database_image_paths,
-        l1_emb=M_V_L1,
         annoy_index=l2_index,
         l_bins=L_BINS,
         a_bins=A_BINS,
@@ -38,4 +31,3 @@ if __name__ == "__main__":
         show=False,
         adjusted_bin_size=True
     )
-    sys.exit()
